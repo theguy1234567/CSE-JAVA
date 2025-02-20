8b47181ba2641c71688e30cdb554b5b0afccf6ab
theguy1234567 <kappanna@gitam.in>
2025-02-20 11:26:28 +0530

    20-02-25

---
+++ lab_exercises/arrays/prog4.java
@@ -0,0 +1,43 @@
+package lab_exercises.arrays;
+
+import java.util.Scanner;
+// You are using Java
+class UniqueElementFinder {
+    //Type your code here
+    public void printUniqueElements(int [] arr){
+        for(int i = 0; i < arr.length;i++){
+            boolean isDuplicate = false;
+            for( int j = 0;j< i;j++){
+                if(arr[i] == arr[j]){
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if(!isDuplicate){
+                System.out.print(arr[i]+" ");
+            }
+        }
+        
+        
+    }
+}
+
+public class prog4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        UniqueElementFinder finder = new UniqueElementFinder();
+        finder.printUniqueElements(arr);
+
+        System.out.println();
+        scanner.close();
+    
+    }
+}
